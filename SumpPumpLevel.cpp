69345ceae1c8861a4ed9abdadeede15af7922dd7
Patrick Jeanson <patrick_jeanson@videotron.ca>
2018-02-25 11:59:25 -0500

    initial

---
+++ SumpPumpLevel.cpp
@@ -0,0 +1,84 @@
+#include "SumpPumpLevel.h"
+
+
+int SumpPumpLevel::getSample()
+{
+	return (4096 - analogRead(mInputPin) );
+}
+
+
+SumpPumpLevel::SumpPumpLevel( unsigned int iSize/*=10*/) : 
+mReadings(),  
+mReadIndex(0),              // the index of the current reading
+mTotal(0),                  // the running mTotal
+mAverage(0),                // the average
+mRawReadPort(A13),
+mInputPin(A3),  //Pin A3
+mMinRead(1023),
+mMaxRead(0),
+mNumReadingsMax(iSize)
+{
+	mReadings.resize(mNumReadingsMax, 0);
+}
+
+
+
+void SumpPumpLevel::execute(void)
+{
+	int lRawRead = 0;
+	int lRead = 0; // Reading remapped from 0 to 100
+	// read from the sensor:
+	lRawRead = getSample();
+	mMinRead = _min(lRawRead, mMinRead);
+	mMaxRead = _max(lRawRead, mMaxRead);
+	// Re-Scale to 0-100%
+	if (mMaxRead - mMinRead != 0)
+	{
+		lRead = map(lRawRead, mMinRead, mMaxRead, 0, 100);
+	}
+	// Remove from total the old sample
+	mTotal -= mReadings[mReadIndex];
+	// add the new reading to the mTotal:
+	mTotal += lRead;
+	//Store the new reading
+	mReadings[mReadIndex] = lRead;
+	// advance to the next position in the array:
+	++mReadIndex;
+	// if we're at the end of the array...
+	if (mReadIndex >= (int)mReadings.size())
+	{
+		// ...wrap around to the beginning:
+		mReadIndex = 0;
+	}
+	// calculate the average:
+	mAverage = mTotal / (int)mReadings.size();
+	// send it to the computer as ASCII digits
+}
+int SumpPumpLevel::getLevel()
+{
+	return mAverage;
+}
+
+String SumpPumpLevel::getDebugStatus()
+{
+	String lMsg="Readings:\n";
+	lMsg += "  min.....:" + String(mMinRead) + "\n";
+	lMsg += "  max.....:" + String(mMaxRead) + "\n";
+	lMsg += "  Total...:" + String(mTotal) + "\n";
+	lMsg += "  Average.:" + String(mAverage) + "\n";
+	for (std::vector<int>::iterator it = mReadings.begin();it != mReadings.end();++it)
+	{
+		lMsg += String(*it) + ",";
+	}
+	lMsg += "\n";
+	return lMsg;
+}
+
+String SumpPumpLevel::getStatus()
+{
+	String lMsg="";
+	lMsg += "  Average.:" + String(mAverage) + "\n";
+
+	lMsg += "\n";
+	return lMsg;
+}
---
+++ SumpPumpLevel.h
@@ -0,0 +1,44 @@
+/* SumpPumpLevel class helps Sump pump level analysis
+*  using Arduino core for ESP8266 WiFi chip
+*  by Patrick Jeanson 
+*  
+*/
+#ifndef SUMPPUMPLEVEL_H
+#define SUMPPUMPLEVEL_H
+#define GS_SERIAL_LOG_1         // Print to Serial only server response
+#define GS_SERIAL_LOG_2       //  Print to Serial client commands and server response
+
+#include "arduino.h"
+#include <vector>
+
+class SumpPumpLevel
+{
+public:
+	SumpPumpLevel( unsigned int iSize=10);
+	int getLevel();
+	void execute(void);
+	String getStatus();
+	String getHtmlStatus();
+	String getError(){return String(" ");}
+	String getDebugStatus();
+protected:
+	
+private:
+	int getSample();
+	// Define the number of samples to keep track of. The higher the number, the
+	// more the readings will be smoothed, but the slower the output will respond to
+	// the input. Using a constant rather than a normal variable lets us use this
+	// value to determine the size of the readings array.
+	unsigned int    mNumReadingsMax;
+	std::vector<int>  mReadings;      // the readings from the analog input
+	int mReadIndex;              // the index of the current reading
+	int mTotal;                  // the running total
+	int mAverage;                // the average
+	int mRawReadPort;
+	int mInputPin;  //Pin A3
+	int mMinRead;
+	int mMaxRead;
+
+
+};
+#endif // G_SENDER
